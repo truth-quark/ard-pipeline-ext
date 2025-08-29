67c36aff5f1b8174ca8c3311ecfc6644a944de18
Ben Davies <bens.public@gmail.com>
2025-08-29 14:53:24 +1000

    Add example code to download Wagga CopDEM tiles.

--- scripts/download_copdem_tiles.sh
+++ scripts/download_copdem_tiles.sh
@@ -16,3 +16,31 @@ for N in {108..120}; do
   mkdir Copernicus_DSM_COG_10_S75_00_W"$N"_00_DEM
   RS gadi:/g/data/v10/eoancillarydata-2/elevation/copernicus_30m_world/Copernicus_DSM_COG_10_S75_00_W"$N"_00_DEM/Copernicus_DSM_COG_10_S75_00_W"$N"_00_DEM.tif  Copernicus_DSM_COG_10_S75_00_W"$N"_00_DEM/
 done
+
+
+
+
+
+# Tiles for the Wagga region
+# download an area with a nested loop
+for LAT in {34..36}; do
+  for LONG in {145..148}; do
+    DEST=Copernicus_DSM_COG_10_S"$LAT"_00_W"$LONG"_00_DEM
+    mkdir $DEST
+    RS gadi:/g/data/v10/eoancillarydata-2/elevation/copernicus_30m_world/"$DEST"/"$DEST".tif  $DEST/
+  done
+done
+
+
+
+function download_wagga () {
+  # Tiles for the Wagga region
+  # download an area with a nested loop
+  for LAT in {34..36}; do
+    for LONG in {145..148}; do
+      DEST=Copernicus_DSM_COG_10_S"$LAT"_00_E"$LONG"_00_DEM
+      mkdir $DEST
+      RS gadi:/g/data/v10/eoancillarydata-2/elevation/copernicus_30m_world/"$DEST"/"$DEST".tif  "$DEST"/
+    done
+  done
+}
